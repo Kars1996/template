641ff18d72977e587409cbc8566eebd0690d2131
Kars1996 <79771285+Kars1996@users.noreply.github.com>
2024-02-21 16:43:29 +0000

    feat: Initialise the template

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+/*
+Copyright © 2024 Kars (github.com/kars1996)
+
+Not to be shared, replicated or used without prior consent.
+Contact Kars for any enquieries
+*/
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <></>
+    </main>
+  );
+}
