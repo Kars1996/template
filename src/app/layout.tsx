8fdad84714059f0aa2f3938d589136e359e55567
Kars1996 <79771285+Kars1996@users.noreply.github.com>
2024-05-18 12:52:01 +0100

    chore(Layout): Remove Depreciated code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,13 +26,6 @@ export default function RootLayout({
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-    // const meta = {
-    //     title: "Kars Templte",
-    //     description: ":D",
-    // };
-
-    // newMeta(meta);
-
     return (
         <html lang="en">
             <head>
