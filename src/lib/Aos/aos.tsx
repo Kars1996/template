f364a8d785187843536a909e371117db0dd4b745
Kars <k@kars.bio>
2024-09-26 12:30:21 +0100

    chore: Formatting

--- src/lib/Aos/aos.tsx
+++ src/lib/Aos/aos.tsx
@@ -1,4 +1,7 @@
 "use client";
+import "./aos.css";
+import Aos from "locomotive-aos";
+import { useEffect } from "react";
 
 /*
 Copyright © 2024 Kars (github.com/kars1996)
@@ -7,16 +10,11 @@ Not to be shared, replicated or used without prior consent.
 Contact Kars for any enquieries
 */
 
-import "./aos.css";
-import Aos from "locomotive-aos";
-import { useEffect } from "react";
-
 export default function () {
     useEffect(() => {
         Aos.init({
             duration: 500,
             easing: "ease-in-out",
-            // once: true,
         });
     }, []);
     return <></>;
