4f08e89d27baae9db9fa138242ffdd97516fd614
Kars <k@kars.bio>
2024-09-28 14:30:26 +0100

    chore: More Spelling Fixes

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -4,7 +4,7 @@ import type { ResponseProp } from "@/types";
 Copyright © 2024 Kars (github.com/kars1996)
 
 Not to be shared, replicated or used without prior consent.
-Contact Kars for any enquieries
+Contact Kars for any enquiries
 */
 
 const Data: ResponseProp = {
