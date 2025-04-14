1a477d6898fe73b4123d41c861714aa457036f99
Kars <k@kars.bio>
2025-04-14 11:15:01 +0100

    chore: example ENV

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -18,6 +18,8 @@ declare global {
     namespace NodeJS {
         interface ProcessEnv {
             NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA: string | undefined;
+            NEXT_PUBLIC_APP_URL: string | undefined;
+            DB_PRISMA_URL: string | undefined;
         }
     }
 }
