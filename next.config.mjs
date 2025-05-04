18c28c3d04b54915bf6874a2ed037dad71e5b411
Kars <k@kars.bio>
2025-05-04 13:42:30 +0100

    chore: image stuff

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,13 @@
 /** @type {import('next').NextConfig} */
-// ? Good luck making sass work in NextJS :D
 const nextConfig = {
-    // webpack: (config) => {
-    //     config.module.rules.push({
-    //         test: /\.scss$/,
-    //         use: [
-    //             "style-loader",
-    //             "css-loader",
-    //             {
-    //                 loader: "sass-loader",
-    //                 options: {
-    //                     sassOptions: {
-    //                         api: "modern",
-    //                         silentDeprecation: ["legacy-js-api"],
-    //                     },
-    //                 },
-    //             },
-    //         ],
-    //     });
-    //     return config;
-    // },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "r2.resynced.design",
+            },
+        ],
+    }
 };
 
 
