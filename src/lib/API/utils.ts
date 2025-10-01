06aa5f338622fe26dcf1b010cc701185911da027
Kars <k@kars.bio>
2025-10-02 00:35:15 +0100

    chore: simplify clientIP stuff

--- src/lib/API/utils.ts
+++ src/lib/API/utils.ts
@@ -3,29 +3,18 @@ import { NextRequest } from "next/server";
 // ? File for API related utils. Could add fingerprinting, location services, etc.
 
 export function getClientIP(request: NextRequest): string {
-  const cfConnectingIp = request.headers.get("cf-connecting-ip");
-  if (cfConnectingIp) {
-    return cfConnectingIp.trim();
-  }
+  const IP_HEADERS = [
+    "cf-connecting-ip",
+    "x-forwarded-for",
+    "x-real-ip",
+    "x-client-ip",
+  ];
 
-  const xForwardedFor = request.headers.get("x-forwarded-for");
-  if (xForwardedFor) {
-    return xForwardedFor.split(",")[0].trim();
-  }
-
-  const xRealIp = request.headers.get("x-real-ip");
-  if (xRealIp) {
-    return xRealIp.trim();
-  }
-
-  const xClientIp = request.headers.get("x-client-ip");
-  if (xClientIp) {
-    return xClientIp.trim();
-  }
-
-  const forwardedFor = request.headers.get("x-forwarded-for");
-  if (forwardedFor) {
-    return forwardedFor.split(",")[0].trim();
+  for (const header of IP_HEADERS) {
+    const value = request.headers.get(header);
+    if (value) {
+      return value.split(",")[0].trim();
+    }
   }
 
   return "127.0.0.1";
